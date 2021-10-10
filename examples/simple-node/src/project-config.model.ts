36a80b42cd52be132aababc7fada639af34807dd
thatkookooguy <neilkalman@gmail.com>
2021-10-10 21:15:42 +0300

    add example

---
+++ examples/simple-node/src/project-config.model.ts
@@ -0,0 +1,13 @@
+import { Exclude, Expose } from 'class-transformer';
+import { IsNumber, Validate, IsString, IsArray } from 'class-validator';
+import { Config, JsonSchema } from '@kibibit/configit';
+
+@Exclude()
+export class ProjectConfig extends Config {
+  @Expose()
+  @IsNumber()
+  @Validate(JsonSchema, [
+    'Server port'
+  ])
+  PORT: number;
+}
---
+++ examples/simple-node/src/test.ts
@@ -0,0 +1,6 @@
+import { ConfigService } from '@kibibit/configit';
+import { ProjectConfig } from './project-config.model';
+
+export const configService = new ConfigService<ProjectConfig>(ProjectConfig);
+
+console.log(configService.config.PORT);
