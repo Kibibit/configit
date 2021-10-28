c2e4e2368adff62ade98f185b4524cc020cac2d2
thatkookooguy <neil.kalman@growthspace.com>
2021-10-28 23:39:24 +0300

    feat(tests): improve coverage
    this is set as a feature to force a new release. I want to check that the release pipeline isn't
    broken because of a previous failure

--- src/config.service.spec.ts
+++ src/config.service.spec.ts
@@ -2,21 +2,35 @@
 import fsExtra from 'fs-extra';
 import { mockProcessExit } from 'jest-mock-process';
 
-import { ConfigService } from './config.service';
-import { PizzaConfig, ToppingEnum } from './pizza.config.model.mock';
+import { ConfigService, IConfigServiceOptions } from './config.service';
+import { PizzaConfig, ToppingEnum, ToppingsConfig } from './pizza.config.model.mock';
 
 class PizzaConfigService extends ConfigService<PizzaConfig> {
-  constructor(passedConfig?: Partial<PizzaConfig>) {
-    super(PizzaConfig, passedConfig);
+  constructor(passedConfig?: Partial<PizzaConfig>, options?: IConfigServiceOptions) {
+    super(PizzaConfig, passedConfig, options);
   }
 }
 
 describe('Config Service', () => {
   let configService: PizzaConfigService;
+  let mockExit;
   beforeAll(() => {
     configService = new PizzaConfigService({
       NODE_ENV: 'test'
     });
+  });
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+    mockExit = mockProcessExit();
+
+    (fsExtra.writeJSONSync as jest.Mock).mockReturnValue(false);
+    (fsExtra.writeFileSync as jest.Mock).mockReturnValue(false);
+    (fsExtra.pathExistsSync as jest.Mock).mockReturnValue(true);
+  });
+
+  afterEach(() => {
+    mockExit.mockRestore();
   });
   test('Service creation', () => {
     expect(configService).toBeDefined();
@@ -38,10 +52,7 @@ describe('Config Service', () => {
   });
 
   test('Service can initialize the config files with saveToFile or init param', () => {
-    (fsExtra.writeJSONSync as jest.Mock).mockReturnValue(false);
     (fsExtra.pathExistsSync as jest.Mock).mockReturnValue(false);
-    const mockExit = mockProcessExit();
-    jest.clearAllMocks();
     new PizzaConfigService({
       saveToFile: true,
       NODE_ENV: 'test'
@@ -51,14 +62,9 @@ describe('Config Service', () => {
     expect((fsExtra.writeJSONSync as jest.Mock).mock.calls[0]).toMatchSnapshot();
     expect((fsExtra.writeJSONSync as jest.Mock).mock.calls[1]).toMatchSnapshot();
     expect(mockExit).toHaveBeenCalledWith(0);
-    mockExit.mockRestore();
   });
 
   test('Service can SAVE the config files with saveToFile or init param', () => {
-    (fsExtra.writeJSONSync as jest.Mock).mockReturnValue(false);
-    (fsExtra.pathExistsSync as jest.Mock).mockReturnValue(true);
-    jest.clearAllMocks();
-    const mockExit = mockProcessExit();
     new PizzaConfigService({
       saveToFile: true,
       NODE_ENV: 'test',
@@ -68,6 +74,137 @@ describe('Config Service', () => {
     expect(fsExtra.writeJSONSync).toHaveBeenCalledTimes(1);
     expect((fsExtra.writeJSONSync as jest.Mock).mock.calls[0]).toMatchSnapshot();
     expect(mockExit).toHaveBeenCalledWith(0);
-    mockExit.mockRestore();
+  });
+
+  test('Service can Save and CONVERT the config files with saveToFile or init param', () => {
+    new PizzaConfigService({
+      saveToFile: true,
+      convert: true,
+      NODE_ENV: 'test',
+      toppings: [ ToppingEnum.Cheese ]
+    });
+
+    expect(fsExtra.writeFileSync).toHaveBeenCalledTimes(1);
+
+    const [ filePath, fileContent ] = (fsExtra.writeFileSync as jest.Mock).mock.calls[0];
+
+    expect(filePath).toMatchSnapshot();
+    expect(`\n${ fileContent }\n`).toMatchSnapshot();
+    expect(mockExit).toHaveBeenCalledWith(0);
+  });
+
+  describe('wrap variables in attribute', () => {
+    test('YAML', () => {
+      new PizzaConfigService({
+        saveToFile: true,
+        convert: true,
+        wrapper: 'env_variables',
+        NODE_ENV: 'test',
+        toppings: [ ToppingEnum.Cheese ]
+      });
+
+      expect(fsExtra.writeFileSync).toHaveBeenCalledTimes(1);
+
+      const [ filePath, fileContent ] = (fsExtra.writeFileSync as jest.Mock).mock.calls[0];
+
+      expect(filePath).toMatchSnapshot();
+      expect(`\n${ fileContent }\n`).toMatchSnapshot();
+      expect(mockExit).toHaveBeenCalledWith(0);
+    });
+
+    test('JSON', () => {
+      new PizzaConfigService({
+        saveToFile: true,
+        wrapper: 'env_variables',
+        NODE_ENV: 'test',
+        toppings: [ ToppingEnum.Cheese ]
+      });
+
+      expect(fsExtra.writeJSONSync).toHaveBeenCalledTimes(1);
+
+      const [ filePath, fileContent ] = (fsExtra.writeJSONSync as jest.Mock).mock.calls[0];
+
+      expect(filePath).toMatchSnapshot();
+      expect(fileContent).toMatchSnapshot();
+      expect(mockExit).toHaveBeenCalledWith(0);
+    });
+  });
+
+  test('Service returns correct empty yaml when config is empty', () => {
+    new PizzaConfigService({
+      saveToFile: true,
+      convert: true
+    });
+
+    expect(fsExtra.writeFileSync).toHaveBeenCalledTimes(1);
+
+    const [ filePath, fileContent ] = (fsExtra.writeFileSync as jest.Mock).mock.calls[0];
+
+    expect(filePath).toMatchSnapshot();
+    expect(fileContent.trim()).toEqual('');
+    expect(mockExit).toHaveBeenCalledWith(0);
+  });
+
+  test('Service can return a plain object', () => {
+    const pizzaConfigInstance = new PizzaConfigService({
+      NODE_ENV: 'test',
+      toppings: [ ToppingEnum.Cheese ]
+    });
+
+    const plainObjectConfig = pizzaConfigInstance.toPlainObject();
+    expect(plainObjectConfig).toMatchSnapshot();
+  });
+
+  test('Service can save yaml with schema', async () => {
+    const pizzaConfigInstance = new PizzaConfigService({
+      NODE_ENV: 'test',
+      toppings: [ ToppingEnum.Cheese ]
+    });
+
+    await pizzaConfigInstance.writeConfigToFile({
+      useYaml: true,
+      excludeSchema: false
+    });
+
+    expect(fsExtra.writeFileSync).toHaveBeenCalledTimes(1);
+
+    const [ filePath, fileContent ] = (fsExtra.writeFileSync as jest.Mock).mock.calls[0];
+
+    expect(filePath).toMatchSnapshot();
+    expect(fileContent).toContain('# yaml-language-server: $schema=');
+    expect(fileContent).toMatchSnapshot();
+  });
+
+  describe('Shared Configurations', () => {
+    test('Can define shared config', () => {
+      const pizzaConfigInstance = new PizzaConfigService({
+        NODE_ENV: 'test',
+        toppings: [ ToppingEnum.Cheese ],
+        INCLUDE_MEAT: true
+      } as any, {
+        sharedConfig: [ ToppingsConfig ]
+      });
+
+      pizzaConfigInstance.writeConfigToFile({
+        useYaml: false
+        // excludeSchema: false
+      });
+
+      expect(fsExtra.writeJSONSync).toHaveBeenCalledTimes(4);
+
+      const [ filePath0, fileContent0 ] = (fsExtra.writeJSONSync as jest.Mock).mock.calls[0];
+      const [ filePath1, fileContent1 ] = (fsExtra.writeJSONSync as jest.Mock).mock.calls[1];
+      const [ filePath2, fileContent2 ] = (fsExtra.writeJSONSync as jest.Mock).mock.calls[2];
+      const [ filePath3, fileContent3 ] = (fsExtra.writeJSONSync as jest.Mock).mock.calls[3];
+
+      expect(filePath0).toMatchSnapshot();
+      expect(fileContent0).toMatchSnapshot();
+      expect(filePath1).toMatchSnapshot();
+      expect(fileContent1).toMatchSnapshot();
+      expect(filePath2).toMatchSnapshot();
+      expect(fileContent2).toMatchSnapshot();
+      expect(filePath3).toMatchSnapshot();
+      expect(fileContent3).toMatchSnapshot();
+    });
   });
 });
